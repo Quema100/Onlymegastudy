7e806e211b5cc53a50ce2a10b4e7a576a17c19cf
Quema100 <myoon0929@gmail.com>
2024-11-10 04:06:39 +0900

    git fix

--- background.js
+++ background.js
@@ -2,3 +2,9 @@ chrome.tabs.onCreated.addListener((tab) => {
     console.log('New Tab:', tab);
     if (tab.pendingUrl === 'chrome://newtab/') return chrome.tabs.update(tab.id, { url: 'https://www.megastudy.net' });
 });
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status !== "complete") return;
+    console.log(tab);
+    if (tab.url === 'chrome://extensions/') return chrome.tabs.update(tab.id, { url: 'https://www.megastudy.net' });
+});
